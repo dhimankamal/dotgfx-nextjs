3b483236407a222a83e11449babd21412ac72a94
kamal <dhimank079@gmai.com>
2022-02-12 13:55:15 +0530

    12022022

--- pages/Components/Navbar.js
+++ pages/Components/Navbar.js
@@ -9,11 +9,12 @@ export default function Navbar () {
         <div className='container'>
           <nav className='navbar navbar-expand-lg'>
             <div className='container-fluid'>
-              <Link href="/">
-                <a className='navbar-brand' >
-                  <Image src={Logo} alt='logo' />
-                </a>{' '}
+              <Link href='/'>
+              <a className='navbar-brand'>
+                <Image src={Logo} alt='logo' />
+              </a>
               </Link>
+              
 
               <button
                 className='navbar-toggler'
@@ -32,18 +33,16 @@ export default function Navbar () {
               >
                 <ul className='navbar-nav ms-auto mb-2 mb-lg-0'>
                   <li className='nav-item'>
-                    <Link href="/home">
-                    <a className='nav-link active' aria-current='page' >
+                    <Link href='/'>
+                    <a className='nav-link' aria-current='page'>
                       Home
                     </a>
                     </Link>
-                    
+                   
                   </li>
                   <li className='nav-item dropdown'>
-                    <Link  href="/">
                     <a
                       className='nav-link dropdown-toggle'
-                     
                       id='navbarDropdown'
                       role='button'
                       data-bs-toggle='dropdown'
@@ -51,41 +50,36 @@ export default function Navbar () {
                     >
                       Template
                     </a>
-                    </Link>
-             
+
                     <ul
                       className='dropdown-menu'
                       aria-labelledby='navbarDropdown'
                     >
                       <li>
-                        <a className='dropdown-item' href="/">
-                          Action
-                        </a>
+                        <a className='dropdown-item'>Action</a>
                       </li>
                       <li>
-                        <a className='dropdown-item' href="/">
-                          Another action
-                        </a>
+                        <a className='dropdown-item'>Another action</a>
                       </li>
                       <li>
                         <hr className='dropdown-divider' />
                       </li>
                       <li>
-                        <a className='dropdown-item' href="/">
-                          Something else here
-                        </a>
+                        <a className='dropdown-item'>Something else here</a>
                       </li>
                     </ul>
                   </li>
                   <li className='nav-item'>
-                    <a className='nav-link' href="/">
-                      About Us
-                    </a>
+                    <Link href='/about'>
+                    <a className='nav-link'>About Us</a>
+                    </Link>
+                    
                   </li>
                   <li className='nav-item'>
-                    <a className='nav-link navbarbutton' href="/">
-                      Contact Us
-                    </a>
+                    <Link href='/contact'>
+                    <a className='nav-link navbarbutton'>Contact Us</a>
+                    </Link>
+                    
                   </li>
                 </ul>
               </div>
